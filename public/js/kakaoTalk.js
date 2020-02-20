9f8021cd16dcf11dfcad5ef656848049c0c3f23b
dooyou <adidas0331@naver.com>
2020-02-20 18:38:44 +0900

    kakaoTalk.js

---
+++ public/js/kakaoTalk.js
@@ -0,0 +1,9 @@
+//<![CDATA[
+// 사용할 앱의 JavaScript 키를 설정해 주세요.
+Kakao.init('24e6586631b7c24ad25487bb20df04d3');
+function chatChannel() {
+  Kakao.Channel.chat({
+  channelPublicId: '_iClgT' // 카카오톡 채널 홈 URL에 명시된 id로 설정합니다.
+  });
+}
+//]]>
